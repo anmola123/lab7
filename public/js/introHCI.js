92f9fbb6f324089ab2337ca6904016492722e906
Anmol Arora <a6arora@ucsd.edu>
2018-02-22 12:25:41 -0800

    lab 7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,11 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	// your code here
+	$(".likeBtn").click(buttonClicked);
+};
+
+function buttonClicked(e) {
+	e.preventDefault();
+	ga('create', 'UA-114604076-1', 'auto');
+	ga('send', 'event', 'like', 'click');
 }
